35df6b518f7ffc92e69f221e2f1ad6fd149791e1
Danilovdenis1105 <danilovdenis479@gmail.com>
2020-09-27 13:30:22 +0300

    edit all section

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,19 @@ $(function () {
 
 
 	$('.featured__slider-inner').slick({
-		dots: true,
+		dots: false,
 		arrows: true,
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/featured/chevron-left.png" alt=""></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><img src="images/featured/chevron-right.png" alt=""></button>',
 		slidesToShow: 1,
 		slidesToScroll: 1,
 	})
 
+	$(".rate-star").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	});
 
 
 
