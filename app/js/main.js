f96a369e12c2273169a05e461d43274ead81b65d
Danilovdenis1105 <danilovdenis479@gmail.com>
2020-09-28 20:08:40 +0300

    add section products and section followers-feed, edit other sections

--- app/js/main.js
+++ app/js/main.js
@@ -11,11 +11,32 @@ $(function () {
 		slidesToScroll: 1,
 	})
 
-	$(".rate-star").rateYo({
+	$('.followers-feed__slider-inner').slick({
+		dots: false,
+		arrows: true,
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/featured/chevron-left.png" alt=""></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><img src="images/featured/chevron-right.png" alt=""></button>',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+	})
+
+	$(".featured__rate-star").rateYo({
 		rating: 5,
 		starWidth: "12px",
 		readOnly: true
 	});
+	$(".followers-feed__item__rate-star").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	});
+	$(".product__rate-star").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true
+	});
+	var mixer = mixitup('.products__inner-box');
+
 
 
 
